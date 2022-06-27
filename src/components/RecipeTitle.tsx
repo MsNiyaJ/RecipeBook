55b814f14fb3094f92f925f55472f2a3818527e7
MsNiyaJ <shaniyasmalcolm@gmail.com>
2022-06-27 00:12:33 -0400

    removed padding-x from title

--- src/components/RecipeTitle.tsx
+++ src/components/RecipeTitle.tsx
@@ -13,7 +13,7 @@ const RecipeTitle = ({ title }) => {
 export default RecipeTitle;
 
 const styles = {
-  containerClass: 'flex justify-center items-center text-center p-5',
+  containerClass: 'flex justify-center items-center text-center py-5',
   titleClass: 'text-xl md:text-3xl',
   barClass: 'bg-black w-1/3 h-px mx-2 max-w-xs',
 };
