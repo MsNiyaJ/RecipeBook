f4f11cd4b93b4f4ff91f7dc8725dc05fb40ec60d
MsNiyaJ <shaniyasmalcolm@gmail.com>
2022-06-27 03:32:54 -0400

    added new icons

---
+++ src/icons/ChevronRight.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ChevronRight = ({ color = 'currentColor' }) => {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke={color}
+        strokeWidth={2}
+      >
+        <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
+      </svg>
+    </div>
+  );
+};
+
+export default ChevronRight;
