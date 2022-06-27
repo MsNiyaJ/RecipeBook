d101df14c261682bc9001e4f5bf15188bec06cbb
MsNiyaJ <shaniyasmalcolm@gmail.com>
2022-06-26 22:36:21 -0400

    navbar is no longer fixed to top of page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = ({ leftContent, centerContent, rightContent }) => {
   return (
-    <div className="bg-red-600 py-4 md:py-8 text-white border-b-4 border-dotted fixed w-full top-0 z-20">
+    <div className="bg-red-600 py-4 md:py-8 text-white w-full">
       <div className="flex justify-between items-center px-2 md:px-20">
         <div id="left">{leftContent}</div>
         <h1
